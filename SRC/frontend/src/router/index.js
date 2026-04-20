441cdb7467e5fa9dc2fb48667a4199de30ec5e7f
AlexCrisTin <nholaian2222@gmail.com>
2026-04-20 20:21:43 +0700

    fix error import

--- SRC/frontend/src/router/index.js
+++ SRC/frontend/src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/auth/LoginView.vue'
-import RegisterView from '@/views/auth/RegisterView.vue'
 import HomeView from '@/views/user/HomeView.vue'
 import BrowseView from '@/views/user/BrowseView.vue'
 import RestaurantDetail from '@/views/user/RestaurantDetail.vue'
@@ -26,8 +24,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/register', name: 'register', component: RegisterView },
 
     // User routes
     { path: '/browse', name: 'browse', component: BrowseView },
